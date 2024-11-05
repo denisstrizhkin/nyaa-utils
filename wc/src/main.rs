87e36e300a3982463d0db572e96b88e61ea755c2
Denis Strizhkin <strdenis02@gmail.com>
2024-11-05 14:54:51 +0300

    it kinda works now

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,28 +1,30 @@
 use clap::{ArgAction, Parser};
 use std::error::Error;
+use std::fs;
 use std::io;
+use std::ops::Add;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[command(name = "wc", version)]
+#[command(version)]
 #[command(about = "word, line, and byte or character count", long_about = None)]
 #[command(disable_help_flag = true, disable_version_flag = true)]
 struct Args {
     /// Write to the stdout the number of characters in each input file
     #[arg(short = 'm')]
-    is_count_char: bool,
+    is_char: bool,
 
     /// Write to the stdout the number of bytes in each input file
     #[arg(short = 'c')]
-    is_count_byte: bool,
+    is_byte: bool,
 
     /// Write to the stdout the number of <newline> characters in each input file
     #[arg(short = 'l')]
-    is_count_line: bool,
+    is_line: bool,
 
     /// Write to the stdout the number of words in each input file
     #[arg(short = 'w')]
-    is_count_word: bool,
+    is_word: bool,
 
     /// Input files
     files: Vec<PathBuf>,
@@ -36,57 +38,79 @@ struct Args {
     version: (),
 }
 
-#[derive(Default)]
 struct Count {
-    chars: Option<u64>,
-    lines: Option<u64>,
-    words: Option<u64>,
+    chars: Option<usize>,
+    lines: Option<usize>,
+    words: Option<usize>,
+}
+
+impl Count {
+    fn new(args: &Args) -> Self {
+        if !(args.is_char || args.is_byte || args.is_line || args.is_word) {
+            Count {
+                lines: Some(0),
+                chars: Some(0),
+                words: Some(0),
+            }
+        } else {
+            Count {
+                lines: if args.is_line { Some(0) } else { None },
+                chars: if args.is_char || args.is_byte {
+                    Some(0)
+                } else {
+                    None
+                },
+                words: if args.is_word { Some(0) } else { None },
+            }
+        }
+    }
+}
+
+impl Add for Count {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            chars: self.chars.zip(other.chars).map(|(a, b)| a + b),
+            words: self.words.zip(other.words).map(|(a, b)| a + b),
+            lines: self.lines.zip(other.lines).map(|(a, b)| a + b),
+        }
+    }
 }
 
 fn count<R: io::BufRead>(reader: R, args: &Args) -> Count {
-    let cnt = if !(args.is_count_char
-        || args.is_count_byte
-        || args.is_count_line
-        || args.is_count_word)
-    {
-        Count {
-            lines: Some(0),
-            chars: Some(0),
-            words: Some(0),
-        }
-    } else {
-        Count {
-            lines: if args.is_count_line { Some(0) } else { None },
-            chars: if args.is_count_char || args.is_count_byte {
-                Some(0)
-            } else {
-                None
-            },
-            words: if args.is_count_word { Some(0) } else { None },
-        }
-    };
-    reader.lines().flatten().fold(cnt, |cnt, line| {
-        let (chars, words) = match (cnt.chars, cnt.words) {
-            (None, None) => (None, None),
-            (chars, words) => line.chars().fold((chars, words), |(chars, words), c| {
-                (
-                    chars.map(|chars| {
-                        if args.is_count_char {
-                            chars + 1
-                        } else {
-                            chars + c.len_utf8() as u64
-                        }
-                    }),
-                    words.map(|_words| 1),
-                )
-            }),
-        };
-        Count {
-            chars,
-            words,
-            lines: cnt.lines.map(|l| l + 1),
-        }
-    })
+    reader
+        .lines()
+        .map_while(Result::ok)
+        .fold(Count::new(&args), |cnt, line| {
+            let chars = cnt.chars.map(|chars| {
+                chars
+                    + line
+                        .chars()
+                        .map(|c| if args.is_char { 1 } else { c.len_utf8() })
+                        .sum::<usize>()
+            });
+            let words = cnt.words.map(|words| {
+                words
+                    + line
+                        .chars()
+                        .fold((0, false), |(words, is_word), c| {
+                            if c.is_whitespace() {
+                                (words, false)
+                            } else if !is_word {
+                                (words + 1, true)
+                            } else {
+                                (words, is_word)
+                            }
+                        })
+                        .0
+            });
+            Count {
+                chars: chars.map(|c| c + 1),
+                words,
+                lines: cnt.lines.map(|l| l + 1),
+            }
+        })
 }
 
 fn print_count(cnt: &Count, name: Option<&str>) {
@@ -112,10 +136,22 @@ fn run() -> Result<(), Box<dyn Error>> {
         let count = count(stdin, &args);
         print_count(&count, None);
     } else {
-        // let total = args.files
-        //     .into_iter()
-        //     .map(|p| fs::File::open(p))
-        //     .map(io::BufReader::new);
+        let total = args
+            .files
+            .iter()
+            .map(fs::File::open)
+            .map_while(Result::ok)
+            .map(io::BufReader::new)
+            .fold(Count::new(&args), |total, br| {
+                total + {
+                    let cnt = count(br, &args);
+                    print_count(&cnt, Some("name"));
+                    cnt
+                }
+            });
+        if args.files.len() > 1 {
+            print_count(&total, Some("total"));
+        }
     }
     Ok(())
 }
